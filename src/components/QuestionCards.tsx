e6170f1e2275e41574bead03416882ef7e5d33d4
ppfurtado <furtado.pedropaulo@gmail.com>
2021-03-29 00:32:22 -0300

    add eslint and prettier

---
+++ src/components/QuestionCards.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const QuestionCards = () => {
+  const person = {
+    name: 'Pedro',
+    idade: 30,
+  };
+
+  return <div></div>;
+};
+
+export default QuestionCards;
